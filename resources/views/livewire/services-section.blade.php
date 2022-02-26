9c8248bad5c7128099db4a3c54c9eee3b7a489c6
David Lopez <dlopez@mattmen.com>
2022-02-26 11:58:25 -0500

    Services Section Modification. Replace Lorem with Text from the previous site.

--- resources/views/livewire/services-section.blade.php
+++ resources/views/livewire/services-section.blade.php
@@ -10,9 +10,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
               </svg>
             </div>
-            <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
+            <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Media Relations</p>
           </dt>
-          <dd class="mt-2 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          <dd class="mt-2 text-base text-gray-500">Your brand is your reputation. With deep knowledge and contacts across the print and online media spaces with journalists, editors and producers, we leverage key influencers to create proactive press opportunities. We shape compelling stories, while building and protecting reputations. We strengthen the media skills of executives with training focused on authenticity and consistent messaging.</dd>
         </div>
   
         <div>
@@ -23,9 +23,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
               </svg>
             </div>
-            <p class="mt-5 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+            <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Public Affairs</p>
           </dt>
-          <dd class="mt-2 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          <dd class="mt-2 text-base text-gray-500">Our top priority is developing trusted brands. We help organizations navigate complex reputational challenges by engaging all stakeholders, from the CEO to consumers, from influencers to government officials. We create and manage key internal and external communications campaigns and programming to elevate your organization's visibility and credibility among stakeholders.</dd>
         </div>
   
         <div>
@@ -36,9 +36,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </div>
-            <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+            <p class="mt-5 text-lg leading-6 font-medium text-gray-900">Marketing</p>
           </dt>
-          <dd class="mt-2 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+          <dd class="mt-2 text-base text-gray-500">Creating captivating content is critical in our ever-evolving world. We use brand storytelling to give life and a personality to an organization. We offer a tailored approach to  marketing and advertising that places an emphasis on the consumer's needs and promotes products and services. Our laser-focused efforts across platforms works to position the organization as a key player and  industry leader.</dd>
         </div>
       </dl>
     </div>
