7bf8a206bd0561176b056ba624b88cc81464a05c
David Lopez <dlopez@mattmen.com>
2022-02-26 11:28:58 -0500

    Hero Section Modification. I changed the tabs on the site. I added the Alphine JS functionality to the mobile model. I added CSS to the section.

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -15,17 +15,17 @@
   ```
 -->
 <section id="hero" class="relative overflow-hidden">
-    <header class="relative">
+    <header x-data="{ open: false }" class="relative">
       <div class="bg-gray-900 pt-6">
         <nav class="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6" aria-label="Global">
           <div class="flex items-center flex-1">
             <div class="flex items-center justify-between w-full md:w-auto">
               <a href="#">
-                <span class="sr-only">Workflow</span>
+                <span class="sr-only">Alta Consulting</span>
                 <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="">
               </a>
               <div class="-mr-2 flex items-center md:hidden">
-                <button type="button" class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white" aria-expanded="false">
+                <button @click="open = ! open"  type="button" class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white" aria-expanded="false">
                   <span class="sr-only">Open main menu</span>
                   <!-- Heroicon name: outline/menu -->
                   <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -35,13 +35,13 @@
               </div>
             </div>
             <div class="hidden space-x-8 md:flex md:ml-10">
-              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Product</a>
+              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Services</a>
   
-              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Features</a>
+              <a href="#" class="text-base font-medium text-white hover:text-gray-300">About</a>
   
-              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Marketplace</a>
+              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Contact</a>
   
-              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Company</a>
+              <a href="#" class="text-base font-medium text-white hover:text-gray-300">Blog</a>
             </div>
           </div>
           <div class="hidden md:flex md:items-center md:space-x-6">
@@ -51,24 +51,22 @@
         </nav>
       </div>
   
-        <!--
-        Mobile menu, show/hide based on menu open state.
-
-        Entering: "duration-150 ease-out"
-            From: "opacity-0 scale-95"
-            To: "opacity-100 scale-100"
-        Leaving: "duration-100 ease-in"
-            From: "opacity-100 scale-100"
-            To: "opacity-0 scale-95"
-        -->
-      <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
+      <div 
+        x-show="open" 
+        x-transition:enter="transition ease-out duration-150 transform"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-100 transform"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95"
+        class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
         <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
           <div class="px-5 pt-4 flex items-center justify-between">
             <div>
               <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
             </div>
             <div class="-mr-2">
-              <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+              <button button @click="open = ! open" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
                 <span class="sr-only">Close menu</span>
                 <!-- Heroicon name: outline/x -->
                 <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -79,13 +77,13 @@
           </div>
           <div class="pt-5 pb-6">
             <div class="px-2 space-y-1">
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Product</a>
+              <a href="#services" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Services</a>
   
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Features</a>
+              <a href="#about" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">About</a>
   
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Marketplace</a>
+              <a href="#contact" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Contact</a>
   
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Company</a>
+              <a href="#blog" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Blog</a>
             </div>
             <div class="mt-6 px-5">
               <a href="#" class="block text-center w-full py-3 px-4 rounded-md shadow bg-indigo-600 text-white font-medium hover:bg-indigo-700">Start free trial</a>
@@ -108,33 +106,14 @@
             {{-- 1ST MAIN COLUMN / MAIN TEXT COLUMN --}}
             <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
               <div class="lg:py-24">
-                <a href="#" class="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
-                  <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">We're hiring</span>
-                  <span class="ml-4 text-sm">Visit our careers page</span>
-                  <!-- Heroicon name: solid/chevron-right -->
-                  <svg class="ml-2 w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                  </svg>
-                </a>
+
+                {{-- Landing Page Heading --}}
                 <h1 class="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
                   <span class="block">A better way to</span>
-                  <span class="block text-indigo-400">ship web apps</span>
+                  <span class="block text-indigo-400">communicate</span>
                 </h1>
-                <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.</p>
-                <div class="mt-10 sm:mt-12">
-                  <form action="#" class="sm:max-w-xl sm:mx-auto lg:mx-0">
-                    <div class="sm:flex">
-                      <div class="min-w-0 flex-1">
-                        <label for="email" class="sr-only">Email address</label>
-                        <input id="email" type="email" placeholder="Enter your email" class="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900">
-                      </div>
-                      <div class="mt-3 sm:mt-0 sm:ml-3">
-                        <button type="submit" class="block w-full py-3 px-4 rounded-md shadow bg-indigo-500 text-white font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900">Start free trial</button>
-                      </div>
-                    </div>
-                    <p class="mt-3 text-sm text-gray-300 sm:mt-4">Start your free 14-day trial, no credit card necessary. By providing your email, you agree to our <a href="#" class="font-medium text-white">terms of service</a>.</p>
-                  </form>
-                </div>
+                <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Alta Consulting provides tailored strategic communications to brands,
+                  C-level executives and organizations.</p>
               </div>
             </div>
 
@@ -149,5 +128,4 @@
         </div>
       </div>
     </main>
-</section>
-  
+  </section>
