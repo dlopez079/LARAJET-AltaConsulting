bbc039ec74326ef2ba70861983e77aeb04fc5662
David Lopez <dlopez@mattmen.com>
2022-03-01 15:23:10 -0500

    Times API. I created the Times API controller using fetch. I'm going to create another controller using Axios.

---
+++ app/Http/Livewire/API/Times.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\API;
+
+use Livewire\Component;
+
+class Times extends Component
+{
+    public function render()
+    {
+        return view('livewire.a-p-i.times');
+    }
+}
