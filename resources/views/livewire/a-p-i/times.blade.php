7e5060d79bd65912198e8ade753b1b4680ff1665
David Lopez <dlopez@mattmen.com>
2022-03-03 12:41:22 -0500

    Times Component Layout Modification. I changed the layout of the Times Component.  I'm going to incorporate a card like approach to list the articles.

--- resources/views/livewire/a-p-i/times.blade.php
+++ resources/views/livewire/a-p-i/times.blade.php
@@ -1,11 +1,10 @@
 <section id="timesAPI">
     <div class="bg-indigo-300 p-5">
-        <h1 class="text-center">New York Times API</h1>
-        <ul type='list'>
-
-            <li>Article 1</li>
-            <li>Article 2</li>
-            <li>Article 3</li>
-        </ul>
+        <div class="section-title">
+            <h1 class="text-center">New York Times News Wire</h1>
+        </div>
+        <div class="section-card truncate">
+            <ul id="newsWire" type='list' class="bg-white color-indigo-300 grid grid-cols-4 gap-4 border shadow rounded"></ul>
+        </div> 
     </div> 
 </section>
